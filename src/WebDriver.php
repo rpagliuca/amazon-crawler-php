ef0396bb7793130caf7a1d965c0e5eb82592e7ca
Rafael Santos <rafael.santos.193@gmail.com>
2018-06-14 01:58:08 -0300

    Added SOCKS proxy configuration

--- src/WebDriver.php
+++ src/WebDriver.php
@@ -4,16 +4,28 @@ namespace RPagliuca\AmazonCrawler;
 
 use \Facebook\WebDriver\Remote\RemoteWebDriver;
 use \Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Chrome\ChromeOptions;
 
 class WebDriver
 {
     public function __construct(
         Configuration $configuration
     ) {
-        $this->configuration = $configuration;
+        $options = new ChromeOptions();
+        $options->addArguments([
+            '--proxy-server=socks4://127.0.0.1:9150',
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+            . '(KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+        ]);
+
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
+
         $this->driver = RemoteWebDriver::create(
-            $this->configuration->get('selenium:host'),
-            DesiredCapabilities::chrome()
+            $configuration->get('selenium:host'),
+            $capabilities,
+            60 * 1000,
+            60 * 1000
         );
         $this->driver->manage()->window()->maximize();
     }
