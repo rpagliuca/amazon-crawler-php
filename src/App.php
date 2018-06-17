33f283fe60bc42a8f7fb2541ea623b6ea83e46d5
Rafael Santos <rafael.santos.193@gmail.com>
2018-06-17 11:42:58 -0300

    feat: Logging improvements

--- src/App.php
+++ src/App.php
@@ -7,16 +7,19 @@ class App
     public function __construct(
         Configuration $configuration,
         QueueFetcher $queueFetcher,
-        ItemProcessor $itemProcessor
+        ItemProcessor $itemProcessor,
+        Logger $logger
     ) {
         $this->configuration = $configuration;
         $this->queueFetcher = $queueFetcher;
         $this->itemProcessor = $itemProcessor;
+        $this->logger = $logger;
     }
 
     public function run()
     {
         $processId = getmypid();
+        $this->logger->setProcessId($processId);
         while (true) {
             $nextItem = $this->queueFetcher->fetchNext($processId);
             if (null !== $nextItem) {
--- src/Logger.php
+++ src/Logger.php
@@ -4,8 +4,14 @@ namespace RPagliuca\AmazonCrawler;
 
 class Logger
 {
+    public function setProcessId($processId)
+    {
+        $this->processId = $processId;
+    }
+
     public function log($message)
     {
+        $message = "[ CRAWLER {$this->processId} ] " . $message;
         error_log($message);
     }
 }
