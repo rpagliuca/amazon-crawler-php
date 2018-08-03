bead03640df9056b3cb270391ad7eb7bd8205c02
Rafael Santos <rafael.santos.193@gmail.com>
2018-08-03 07:16:26 -0300

    feat: Move setup to bin/console

--- setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require 'bootstrap.php';
-
-$app = $container->get('RPagliuca\AmazonCrawler\Setup');
-$app->run();
---
+++ src/Console/InitialDataInitializerCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace RPagliuca\AmazonCrawler\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class InitialDataInitializerCommand extends Command
+{
+    public function __construct(
+        \RPagliuca\AmazonCrawler\Setup $instance
+    ) {
+        $this->instance = $instance;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('setup:insert-initial-data')
+            ->setDescription('Insert initial database rows for node 1')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $response = $this->instance->run();
+        $output->writeln($response);
+    }
+}
