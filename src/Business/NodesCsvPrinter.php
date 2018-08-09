77614b569a144772ac785c78abf844654fcbe56c
Rafael Santos <rafael.santos.193@gmail.com>
2018-08-09 07:32:50 -0300

    feat: Add url to node csv

--- src/Business/NodesCsvPrinter.php
+++ src/Business/NodesCsvPrinter.php
@@ -6,18 +6,25 @@ class NodesCsvPrinter
 {
     public function __construct(
         \Doctrine\ORM\EntityManager $em,
-        \RPagliuca\AmazonCrawler\System\ArrayToCsvConverter $arrayToCsvConverter
+        \RPagliuca\AmazonCrawler\System\ArrayToCsvConverter $arrayToCsvConverter,
+        \RPagliuca\AmazonCrawler\Configuration $configuration
     ) {
         $this->db = $em->getConnection();
         $this->arrayToCsvConverter = $arrayToCsvConverter;
+        $this->configuration = $configuration;
     }
 
     public function print()
     {
-        $st = $this->db->query('
-            SELECT id, title, REPLACE(REPLACE(price, "R$ ", ""), ",", ".") AS price FROM Node
+        $baseUrl = $this->configuration->get('target:domain');
+        $st = $this->db->prepare('
+            SELECT id, title,
+            REPLACE(REPLACE(price, "R$ ", ""), ",", ".") AS price,
+            CONCAT(:baseUrl, url) AS url FROM Node
             WHERE Node.title IS NOT NULL
         ');
+        $st->bindValue(':baseUrl', $baseUrl);
+        $st->execute();
         $results = $st->fetchAll(\PDO::FETCH_ASSOC);
         return $this->arrayToCsvConverter->convert($results);
     }
